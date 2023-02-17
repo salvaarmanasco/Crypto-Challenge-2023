cc9a85ce64b28cff16d80b495937ee4fd14bd433
Salvaarmanasco <salvador.armanasco@gmail.com>
2023-02-17 18:50:26 -0300

    Correction-ScreenHome

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -5,15 +5,6 @@ import axios from 'axios';
 import AddButton from './components/AddButton';
 
 const Home = () => {
-  useEffect(() => {
-    const consultAPI = async () => {
-      const url = 'https://data.messari.io/api/v1/assets/btc/metrics';
-      const result = await axios.get(url);
-      console.log(result);
-    };
-    consultAPI();
-  }, []);
-
   return (
     <>
       <Header />
